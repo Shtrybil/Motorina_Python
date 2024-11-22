828fefe825a68c2f9a7c2ffe958d63f36b913aa0
motol <motolisochka@gmail.com>
2024-11-22 04:56:06 +0300

    к пз 5

---
+++ PZ_5/pz_5_1.py
@@ -0,0 +1,24 @@
+# Составить программу, в которой функция генерирует четырехзначное число и
+# определяет, есть ли в числе одинаковые цифры.
+import random
+
+def generate_number():
+    while True:
+        number = random.randint(1000, 9999)
+        try:
+            digits = set()
+            for digit in str(number):
+                if digit in digits:
+                    return number
+                else:
+                    digits.add(digit)
+            return False
+        except ValueError:
+            pass
+
+result = generate_number()
+
+if result:
+    print("В числе есть одинаковые цифры:", result)
+else:
+    print("Все цифры в числе различны")
