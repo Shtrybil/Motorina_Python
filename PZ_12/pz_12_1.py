2d06d52dd2ce7fc9faa392f2611e2c59a8780102
motol <motolisochka@gmail.com>
2025-04-25 17:57:38 +0300

    changed

--- PZ_12/pz_12_1.py
+++ PZ_12/pz_12_1.py
@@ -1,16 +1,16 @@
-#В последовательности на n целых элементов найти произведение элементов
-# средней трети
+#В последовательности на n целых элементов найти произведение элементов средней трети
 import  random
 n = int(input("введите число n: "))
 lst = []
-for num in range(n):
-    num = random.randint(1, 50)
-    lst.append(num)
+lst = [random.randint(-5, 10) for i in range(n)]
+
 print(lst)
 
 start = n // 3
 end = 2 * n // 3
 lst2 = [lst[i] for i in range(start, end)]
+
+
 from functools import reduce
 result= reduce(lambda x, y: x * y, lst2, 1)
 print(f"Произведение средней трети: {result}")
