cf9f5d75db022d639b26e8e1b4e9042098dd8185
motol <motolisochka@gmail.com>
2025-04-26 11:20:43 +0300

    changed

--- PZ_14/pz_14_1.py
+++ PZ_14/pz_14_1.py
@@ -1,15 +1,11 @@
 # Из исходного текстового файла (expansion.txt) выбрать имена фалов,
-# соответствующие типам: .xls, .xml, .html, .css, .py. Посчитать количество полученных
-# элементов.
+# соответствующие типам: .xls, .xml, .html, .css, .py. Посчитать количество полученных элементов.
 import re
 with open('expansion.txt', 'r', encoding='utf-8') as file:
     f = file.read()
 
 reg = r'\b\w+\.(?:xls|xml|html|css|py)\b'
-
-# Поиск всех совпадений
 files = re.findall(reg, f)
-
 count = len(files)
 
 print("Найденные файлы:", files)
